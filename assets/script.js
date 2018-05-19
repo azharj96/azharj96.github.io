a9e8b95d8e976531a489141cf67991be7fdbb468
Jonas Mueller <sanjom96@gmail.com>
2018-05-20 00:30:19 +0100

    Added initial website

---
+++ assets/script.js
@@ -0,0 +1,20 @@
+const contact = document.getElementById('contact');
+const items = document.getElementById('items');
+
+contact.innerHTML = `<a href="${data.contact.url}"><span>${data.contact.text}</span></a>`;
+
+items.innerHTML = data.items.map(function (item) {
+  const title = item.url
+    ? `<h2 class="title"><a href="${item.url}">${item.title}</a></h2>`
+    : `<h2 class="title">${item.title}</h2>`;
+  return `<div class="item">
+    <div class="meta">
+      <div class="category"><span>${item.category}</span></div>
+      <div class="date"><span>${item.date}</span></div>
+    </div>
+    <div class="content">
+      ${title}
+      <p class="summary">${item.summary}</p>
+    </div>
+  </div>`;
+}).join('');
---
+++ data.js
@@ -0,0 +1,36 @@
+let data = {
+  contact: {
+    text: 'Contact',
+    url: 'mailto:azharj96@gmail.com'
+  },
+  items: [
+    {
+      category: 'Research',
+      date: '2018',
+      title: 'Financial Crisis Prediction with Econophysics',
+      summary: 'Paper describing the computational modelling of an investing population to simulate financial crisis',
+      url: undefined
+    },
+    {
+      category: 'Business',
+      date: '2018',
+      title: 'Currant - A Global Satisfaction Network',
+      summary: 'A token leveraging blockchain technology as an incremental reward system for performance in the workplace',
+      url: 'http://azharjamal.com'
+    },
+    {
+      category: 'Research',
+      date: '2018',
+      title: 'Using an LSTM for Stock Price Forecasting',
+      summary: 'A token leveraging blockchain technology as an incremental reward system for performance in the workplace',
+      url: undefined
+    },
+    {
+      category: 'Startup',
+      date: '2017',
+      title: 'DoIDate',
+      summary: 'A description of the thing would go here to tantalise the reader',
+      url: undefined
+    }
+  ]
+};
